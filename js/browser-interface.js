32f0a92f5a47292942726a8156a99056b5f483d5
Adam Craig <lateralusajc@gmail.com>
2016-05-27 12:17:05 -0700

    Add repository language and image to each outputted repo, add link to image that redirects to specific repo URL, add SASS styling

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,5 +1,28 @@
 var user;
 var git = require('../js/git.js');
+var icon = "<span class='glyphicon glyphicon-cloud-download'></span>";
+
+var displayFunction = function(repos) {
+    $('.outputHeader').text("Here's a list of all repos listed under the user " + user + ":");
+
+    repos.forEach(function(repo) {
+      $('.repoList').append(
+      "<div class='col-xs-4 repoBox'>" +
+        "<a href='" + repo.repoUrl + "'>" +
+          "<img class='repoIcon' src='img/repoIcon.png'>" +
+        "</a>" + "<br>" +
+        "<br>" + "<strong>Repository Name: </strong>" + repo.repoName +
+        "<br>" + "<strong>Description: </strong>" + repo.repoDescription +
+        "<br>" + "<strong>Primary Language: </strong>" + repo.repoLanguage +
+      "</div>"
+      );
+      console.log(repo.repoUrl);
+    });
+};
+
+var errorDisplayFunction = function() {
+  $('.outputHeader').text("No repos found. Try another search above!");
+};
 
 $(document).ready(function(){
   $('#inputBox').submit(function(event){
@@ -11,6 +34,6 @@ $(document).ready(function(){
     $('.repoList').empty();
     $('.outputHeader').empty();
 
-    git.getReposAndDisplay(user);
+    git.getReposAndDisplay(user, displayFunction, errorDisplayFunction);
   });
 });
--- js/git.js
+++ js/git.js
@@ -1,9 +1,30 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getReposAndDisplay = function(user) {
-  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+var repos = [];
+
+var Repo = function(repoName, repoDescription, repoLanguage, repoUrl) {
+  this.repoName = repoName;
+  this.repoDescription = repoDescription;
+  this.repoLanguage = repoLanguage;
+  this.repoUrl = repoUrl;
+};
+
+exports.getReposAndDisplay = function(user, displayFunction, errorDisplayFunction) {
+  repos = [];
+  $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
+
+    $('.loadingImage').hide();
     console.log(response);
+    response.forEach(function(repo) {
+      var newRepo = new Repo(repo.name, repo.description, repo.language, repo.html_url);
+      repos.push(newRepo);
+    });
+
+    displayFunction(repos);
+
   }).fail(function(error){
+    $('.loadingImage').hide();
     console.log(error.responseJSON.message);
+    errorDisplayFunction();
   });
 };
