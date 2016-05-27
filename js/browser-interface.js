a1bc07a007d531dc23d866f8359657cde3d02bb0
Adam Craig <lateralusajc@gmail.com>
2016-05-27 09:24:46 -0700

    Add basic API functionality that correctly prints the API response for a given user to the console

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,5 +1,16 @@
+var user;
+var git = require('../js/git.js');
 
 $(document).ready(function(){
+  $('#inputBox').submit(function(event){
+    event.preventDefault();
+    user = $('#userInput').val();
+    $('#userInput').val("");
 
+    $('.loadingImage').show();
+    $('.repoList').empty();
+    $('.outputHeader').empty();
 
+    git.getReposAndDisplay(user);
+  });
 });
